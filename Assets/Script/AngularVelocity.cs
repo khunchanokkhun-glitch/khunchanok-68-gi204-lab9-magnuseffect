33ac48778e98c347c4836e09421b1db5eb751b94
khunchanokkhun-glitch <khunchanok.khun@bumail.net>
2026-03-10 16:07:18 +0700

    add script if no key

--- Assets/Script/AngularVelocity.cs
+++ Assets/Script/AngularVelocity.cs
@@ -1,23 +1,48 @@
+using System;
+
 using UnityEngine;
+
 using UnityEngine.InputSystem;
 
-public class AngularVelocity : MonoBehaviour
+public class AngularVelovity : MonoBehaviour
+
 {
-    public float angularVelocitySpeed;
+
+    public float angularSpeed;
+
     Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
+
     {
+
         rb = GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
     void FixedUpdate()
+
     {
-        if (Keyboard.current.aKey.isPressed) 
+
+        if (Keyboard.current.aKey.isPressed)
+
         {
-            rb.angularVelocity = new Vector3(0, angularVelocitySpeed, 0);
+
+            rb.angularVelocity = new Vector3(0, angularSpeed, 0);
+
         }
+
+        else
+
+        {
+
+            rb.angularVelocity = Vector3.zero;
+
+        }
+
     }
+
 }
+
